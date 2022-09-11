99d50b2c61f612cbd3b64062da7f1c1d56ee8200
ssd <2145619745@qq.com>
2022-09-11 16:55:58 +0800

    版本1

---
+++ godpleaseNoooooooooooo.java
@@ -0,0 +1,18 @@
+class Dog{
+	public static void main(String[] args){
+		final double pi = 3.1415926;
+		long i = 44444446;
+		i += 2;
+		int j = (int)i;
+		j = j+2;
+		System.out.println(j>i&i==++i?1:i);
+		System.out.println(j<i&&i++==3);
+		System.out.println(""+i);
+		System.out.println("pi"+"="+pi);
+		System.out.println("i"+"="+ i++);
+		System.out.println("i2"+"=" +i);
+		System.out.println("j"+"="+ j);
+		
+			
+}
+} 
